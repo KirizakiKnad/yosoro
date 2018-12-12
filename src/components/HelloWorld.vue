1f3c2538d910e7753bc14a6b59b4e4ba18bf64df
Yuki <45788585+KirizakiKnad@users.noreply.github.com>
2018-12-12 15:45:31 +0800

    firstUP

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <button v-on:click="clickHandler('dadadadada')" type="button" name="button">按钮</button>
+    <p v-show="flag" > 123 </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      flag:true
+    }
+  },
+  methods:{
+    clickHandler(data){
+      console.log(data);
+      this.flag=!this.flag;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
