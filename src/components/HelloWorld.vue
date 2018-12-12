00655a3752f36bb7579eb532579285e370ca575a
Morkin_AG <1132217922@qq.com>
2018-12-12 17:27:36 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <button v-on:click="clickHandler('dadadadada')" type="button" name="button">按钮</button>
-    <p v-show="flag" > 123 </p>
+    <p v-show="flag" > 321 </p>
   </div>
 </template>
 
